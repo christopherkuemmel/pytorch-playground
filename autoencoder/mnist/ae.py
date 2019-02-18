e29f437c4b974dd7300409736d89ba9f224cb614
Christopher Kümmel <kuemmel.christopher@gmail.com>
2019-02-18 14:41:17 +0100

    add gitkeep to make sure folders exist

--- autoencoder/mnist/ae.py
+++ autoencoder/mnist/ae.py
@@ -30,7 +30,7 @@ class AE(nn.Module):
 # define normalize transformation
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-# download / create MNIST dataset
+# download / create MNIST dataset (also works for FashionMNIST)
 train_set = torchvision.datasets.MNIST('./autoencoder/mnist/data', download=True, transform=transform)
 test_set = torchvision.datasets.MNIST('./autoencoder/mnist/data', train=False, download=True, transform=transform)
 
